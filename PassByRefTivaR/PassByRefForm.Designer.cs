71bbfc04dc0893cec0b1638e203e9b30ec73a28d
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-04-24 14:28:10 -0400

    Initial commit

---
+++ PassByRefTivaR/PassByRefForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PassByRefTivaR
+{
+    partial class frmPassByRef
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAboveText = new System.Windows.Forms.Label();
+            this.picImages = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImages)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAboveText
+            // 
+            this.lblAboveText.AutoSize = true;
+            this.lblAboveText.Location = new System.Drawing.Point(99, 30);
+            this.lblAboveText.Name = "lblAboveText";
+            this.lblAboveText.Size = new System.Drawing.Size(138, 13);
+            this.lblAboveText.TabIndex = 0;
+            this.lblAboveText.Text = "Click on image to change it.";
+            this.lblAboveText.MouseLeave += new System.EventHandler(this.lblAboveText_MouseLeave);
+            this.lblAboveText.MouseHover += new System.EventHandler(this.lblAboveText_MouseHover);
+            // 
+            // picImages
+            // 
+            this.picImages.Image = global::PassByRefTivaR.Properties.Resources.AC;
+            this.picImages.Location = new System.Drawing.Point(74, 78);
+            this.picImages.Name = "picImages";
+            this.picImages.Size = new System.Drawing.Size(191, 261);
+            this.picImages.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImages.TabIndex = 1;
+            this.picImages.TabStop = false;
+            // 
+            // frmPassByRef
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 351);
+            this.Controls.Add(this.picImages);
+            this.Controls.Add(this.lblAboveText);
+            this.Name = "frmPassByRef";
+            this.Text = "Pass By Reference By Tiva Rait";
+            ((System.ComponentModel.ISupportInitialize)(this.picImages)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAboveText;
+        private System.Windows.Forms.PictureBox picImages;
+    }
+}
+
---
+++ PassByRefTivaR/PassByRefForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassByRefTivaR
+{
+    public partial class frmPassByRef : Form
+    {
+        // Declare global variables 
+        Random randNumberGenerator = new Random();
+
+        public frmPassByRef()
+        {
+            InitializeComponent();
+        }
+
+        // Procedure: ChangeCard
+        // Input: ref PictureBox a PictureBox, int cardNumber
+        // Output: void
+        // Description: This procedure takes a picture box passed by reference and a card number
+        //  It assigns the image in the picture box to card with a given card number.
+        private void ChangeCard (ref PictureBox aPictureBox, int cardNumber)
+        {
+            if (cardNumber == 1)
+            {
+                aPictureBox.Image = Properties.Resources.AH;
+            }
+            else if ( cardNumber == 2)
+            {
+                aPictureBox.Image = Properties.Resources.AS;
+            }
+            else if (cardNumber == 3)
+            {
+                aPictureBox.Image = Properties.Resources.AD;
+            }
+        }
+
+        private void lblAboveText_MouseHover(object sender, EventArgs e)
+        {
+            lblAboveText.Text = "I am Hovering :)";
+        }
+
+        private void lblAboveText_MouseLeave(object sender, EventArgs e)
+        {
+            lblAboveText.Text = "Click on Image to change it.";
+        }
+    }
+}
